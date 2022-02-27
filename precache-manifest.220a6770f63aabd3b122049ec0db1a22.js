4e1d51ec0aab72cf2e5da590b344680af8f8af25
Ralph Mari Gabrinao <gabrinao.ralph@gmail.com>
2022-02-27 15:53:58 -0800

    Updates

---
+++ precache-manifest.220a6770f63aabd3b122049ec0db1a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0329355e5f43a4406dfd971b36204ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d74b5eef98531b0533c",
+    "url": "/static/css/2.34178e39.chunk.css"
+  },
+  {
+    "revision": "e3c6257815aa229defb9",
+    "url": "/static/css/main.0ed4f152.chunk.css"
+  },
+  {
+    "revision": "6d74b5eef98531b0533c",
+    "url": "/static/js/2.cea02efc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.cea02efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c6257815aa229defb9",
+    "url": "/static/js/main.563279fe.chunk.js"
+  },
+  {
+    "revision": "26277769c14bf58983f7",
+    "url": "/static/js/runtime-main.5bd0360d.js"
+  }
+]);
